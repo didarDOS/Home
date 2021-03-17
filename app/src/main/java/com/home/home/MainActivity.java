b57fbd24aede48f48627c67994277736da5232f9
didarDOS <didar9516@gmail.com>
2021-03-17 18:50:28 +0600

    Created GetStarted activity

--- app/src/main/java/com/home/home/MainActivity.java
+++ app/src/main/java/com/home/home/MainActivity.java
@@ -2,13 +2,29 @@ package com.home.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void getstarted(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
