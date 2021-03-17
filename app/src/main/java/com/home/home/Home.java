44801ca205433a862cf91234a2ffbc6ab86e0108
dos <didar9516@gmail.com>
2021-03-17 23:27:16 +0600

    Added Home activity_home

--- app/src/main/java/com/home/home/Home.java
+++ app/src/main/java/com/home/home/Home.java
@@ -3,12 +3,34 @@ package com.home.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
+
+    CarouselView carouselView;
+    int[] sampleImages = {R.drawable.bedroom, R.drawable.living_room};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        carouselView.setImageListener(imageListener);
+
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
 }
